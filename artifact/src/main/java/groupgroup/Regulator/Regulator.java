8acd5b4841e4564711e79cdd3e455e4502022fb2
NastyFever <k-lime@hotmail.com>
2016-03-17 10:29:53 +0100

    Build simple (likely not good) logic for estimating return time with TCR

--- artifact/src/main/java/groupgroup/Regulator/Regulator.java
+++ artifact/src/main/java/groupgroup/Regulator/Regulator.java
@@ -2,8 +2,6 @@ package groupgroup.Regulator;
 
 import org.json.simple.JSONObject;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Regulator {
     long numberOfReleasedTokens;
     long numberOfUsedTokens;
@@ -12,7 +10,7 @@ public class Regulator {
     final int HWM;
     final int AM;
 
-    int currentLevelOfQueue;
+    long currentLevelOfQueue;
 
     public Regulator(int LWM, int HWM, int AM) {
         this.numberOfReleasedTokens = 0L;
@@ -21,25 +19,35 @@ public class Regulator {
         this.HWM = HWM;
         this.AM = AM;
         this.currentLevelOfQueue = 0;
+        this.lastTimeForUpdate = System.currentTimeMillis();
+        this.taskCompletionRate = -1;
     }
 
     public synchronized JSONObject handleRequest(int numberOfRetries) {
-        long accessToken = ++numberOfReleasedTokens;
-        long returnTime = 0;
-
         JSONObject jc = new JSONObject();
         if(currentLevelOfQueue < AM) {
             jc.put("Type", "AccessService");
+            long accessToken = ++numberOfReleasedTokens;
             jc.put("Token", accessToken);
         } else {
             jc.put("Type", "ScheduleMessage");
+            double returnTime = System.currentTimeMillis() + 1/taskCompletionRate;
             jc.put("ReturnTime", returnTime);
         }
         return jc;
     }
 
+    long lastTimeForUpdate;
+    double taskCompletionRate;
+
     public synchronized void recievedUpdateFromApplicationServer(long numberOfUsedTokens) {
+        long currentTime = System.currentTimeMillis();
+        if(numberOfUsedTokens > this.numberOfUsedTokens) {
+            long newFinishedJobs = numberOfUsedTokens - this.numberOfUsedTokens;
+            taskCompletionRate = newFinishedJobs / (currentTime- lastTimeForUpdate);
+            System.out.println("TCR is updated to: " + taskCompletionRate);
+        }
         this.numberOfUsedTokens = numberOfUsedTokens;
-        currentLevelOfQueue = (int) (numberOfReleasedTokens - this.numberOfUsedTokens);
+        currentLevelOfQueue = numberOfReleasedTokens - this.numberOfUsedTokens;
     }
 }
