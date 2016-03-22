25fe883b14ac5132a0702e761f97bbebadff306f
Johan Persson <johan.persson@qmatic.com>
2016-03-22 11:56:01 +0100

    Switch to Logback for logging

--- artifact/src/main/java/groupgroup/Regulator/Regulator.java
+++ artifact/src/main/java/groupgroup/Regulator/Regulator.java
@@ -3,15 +3,16 @@ package groupgroup.Regulator;
 import groupgroup.Main;
 import org.json.simple.JSONObject;
 
-import java.io.IOException;
+/*import java.io.IOException;
 import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+import java.util.logging.SimpleFormatter;*/
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Regulator {
 
-    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
-    private FileHandler handler;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class.getName());
 
     long numberOfFinishedJobs;
     Algorithm algorithm;
@@ -19,19 +20,22 @@ public class Regulator {
     public Regulator(int LWM, int HWM, int AM) {
         this.numberOfFinishedJobs = 0L;
         this.algorithm = new FirstVersionAlgorithm(LWM, HWM, AM);
-        init();
+//        init();
+        LOGGER.debug("HEJ");
     }
 
-    private void init() {
+/*    private void init() {
         try {
-            handler = new FileHandler("log.txt", true);
+            //TODO: fix file appending???
+//            handler = new FileHandler("log.txt", true);
+            appender = new FileAppender()
         } catch (IOException e) {
             System.out.println("Failed to create file log.txt");
         }
         SimpleFormatter formatter = new SimpleFormatter();
         handler.setFormatter(formatter);
         LOGGER.addHandler(handler);
-    }
+    }*/
 
     public JSONObject handleRequest(int numberOfRetries) {
         JSONObject jc = algorithm.runAlgorithm(numberOfFinishedJobs, numberOfRetries);
