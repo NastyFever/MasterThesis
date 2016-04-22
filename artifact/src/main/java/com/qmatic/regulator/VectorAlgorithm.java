cd50b5ce0750d33596fedd738b95287014d0145f
NastyFever <k-lime@hotmail.com>
2016-04-22 16:04:39 +0200

    Add method for deciding on which queue to pick

--- artifact/src/main/java/com/qmatic/regulator/VectorAlgorithm.java
+++ artifact/src/main/java/com/qmatic/regulator/VectorAlgorithm.java
@@ -1,23 +1,66 @@
 package com.qmatic.regulator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class VectorAlgorithm extends SecondVersionAlgorithm {
 
-    int[] queues = {0, 0, 0, 0, 0, 0, 0, 0};
+    long[] queues = {0, 0, 0, 0, 0, 0, 0, 0};
 
+    public HashMap<Integer, Integer> lookUpMap = new HashMap<>();
 
     public VectorAlgorithm(int LWM, int HWM, int AM, double initialTCR){
         super(LWM, HWM, AM, initialTCR);
+        buildLookUpMap();
+    }
+
+    private void buildLookUpMap() {
+        for(int i = 0; i < queues.length; i++) {
+            lookUpMap.put((int) Math.pow(2, i), i);
+        }
     }
 
     @Override
     public double getReturntime() {
-        ArrayList<Integer> queueIds = computeQueues(estimatedTaskCompletionRatePerMillis);
-        return 0;
+        ArrayList<Integer> queueIds = computeWhatQueuesToFill(estimatedTaskCompletionRatePerMillis);
+        long relativeWaitTime = placeInVirtualQueueAndGetWaitTime(queueIds);
+        return relativeWaitTime;
     }
 
-    protected ArrayList<Integer> computeQueues(double wantedComebackRate) {
+    protected synchronized int placeInVirtualQueueAndGetWaitTime(ArrayList<Integer> queueIds) {
+        long currentTime = getCurrentTimeInMillis();
+        // First check if any of the queues is empty, if so return fastest queue
+        for(int queueRate : queueIds) {
+            long queueEndTime = queues[lookUpMap.get(queueRate)];
+            if (queueEndTime < currentTime) {
+                queues[lookUpMap.get(queueRate)] = currentTime
+                        + convertRateFromPerSecondToIntervalInMillis(queueRate);
+                return queueRate;
+            }
+        }
+        // Second, if non of the queues where empty, add to the shortest
+        long compareTo = Long.MAX_VALUE;
+        int choosedQueueRate = -1;
+        for(int queueRate : queueIds) {
+            long queueEndTime = queues[lookUpMap.get(queueRate)];
+            if(compareTo > queueEndTime) {
+                compareTo = queueEndTime;
+                choosedQueueRate = queueRate;
+            }
+        }
+        queues[lookUpMap.get(choosedQueueRate)] += convertRateFromPerSecondToIntervalInMillis(choosedQueueRate);
+        return choosedQueueRate;
+    }
+
+    private int convertRateFromPerSecondToIntervalInMillis(int choosedQueueRate) {
+        return (int) (1000 * (1.0/choosedQueueRate));
+    }
+
+    protected long getCurrentTimeInMillis() {
+        return System.currentTimeMillis();
+    }
+
+    protected ArrayList<Integer> computeWhatQueuesToFill(double wantedComebackRate) {
         ArrayList<Integer> whatQueuesToPick = new ArrayList<>();
         int rest = (int) Math.ceil(1000*wantedComebackRate); // aimed value for return rate
 
--- artifact/src/main/test/java/com/qmatic/regulator/VectorAlgorithmTest.java
+++ artifact/src/main/test/java/com/qmatic/regulator/VectorAlgorithmTest.java
@@ -16,22 +16,96 @@ public class VectorAlgorithmTest {
     }
 
     @Test
-    public void testComputeQueues() throws Exception {
+    public void testComputeWhatQueuesToFill() throws Exception {
         int HWM = 400, LWM = 0, AM = 300;
         double INITIAL_TCR = 8;
         VectorAlgorithm alg = Mockito.spy(new VectorAlgorithm(LWM, HWM, AM, INITIAL_TCR));
 
         ArrayList<Object> expectedFirstAssertReturn = new ArrayList<>();
         expectedFirstAssertReturn.add(8);
-        assertThat(alg.computeQueues(0.008), is(expectedFirstAssertReturn));
+        assertThat(alg.computeWhatQueuesToFill(0.008), is(expectedFirstAssertReturn));
         ArrayList<Object> expectedSecondAssertReturn = new ArrayList<>();
         expectedSecondAssertReturn.add(4);
         expectedSecondAssertReturn.add(2);
         expectedSecondAssertReturn.add(1);
-        assertThat(alg.computeQueues(0.007), is(expectedSecondAssertReturn));
+        assertThat(alg.computeWhatQueuesToFill(0.007), is(expectedSecondAssertReturn));
         ArrayList<Object> expectedThirdAssertReturn = new ArrayList<>();
         expectedThirdAssertReturn.add(8);
         expectedThirdAssertReturn.add(1);
-        assertThat(alg.computeQueues(0.009), is(expectedThirdAssertReturn));
+        assertThat(alg.computeWhatQueuesToFill(0.009), is(expectedThirdAssertReturn));
     }
+
+    @Test
+    public void testChooseQueueId() throws Exception {
+        int HWM = 400, LWM = 0, AM = 300;
+        double INITIAL_TCR = 8;
+        VectorAlgorithm alg = Mockito.spy(new VectorAlgorithm(LWM, HWM, AM, INITIAL_TCR));
+        testBasicBehaviourForChooseQueueId(alg);
+        alg = Mockito.spy(new VectorAlgorithm(LWM, HWM, AM, INITIAL_TCR));
+        testPlaceInVirtualQueueAndGetWaitTime(alg);
+
+    }
+
+    private void testBasicBehaviourForChooseQueueId(VectorAlgorithm alg) {
+        Mockito.when(alg.getCurrentTimeInMillis()).thenReturn((long) 100);
+
+        ArrayList<Integer> queuesToPickFrom = new ArrayList<>();
+        queuesToPickFrom.add(4);
+        queuesToPickFrom.add(2);
+        queuesToPickFrom.add(1);
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(4));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(2));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(1));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(4));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(4));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(2));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(4));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(4));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(2));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(1));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(4));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(4));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(2));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(4));
+    }
+
+    private void testPlaceInVirtualQueueAndGetWaitTime(VectorAlgorithm alg) {
+        Mockito.when(alg.getCurrentTimeInMillis()).thenReturn((long) 100);
+
+        ArrayList<Integer> queuesToPickFrom = new ArrayList<>();
+        queuesToPickFrom.add(4);
+        queuesToPickFrom.add(2);
+        queuesToPickFrom.add(1);
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(4));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(2));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(1));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(4));
+
+
+        queuesToPickFrom = new ArrayList<>();
+        queuesToPickFrom.add(8);
+        queuesToPickFrom.add(2);
+        queuesToPickFrom.add(1);
+
+
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(2));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(2));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(1));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+        assertThat(alg.placeInVirtualQueueAndGetWaitTime(queuesToPickFrom), is(8));
+    }
+
+
 }
